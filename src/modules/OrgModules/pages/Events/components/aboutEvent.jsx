cbea162da683dc7a380c6e8e8b1a41b0566c370a
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2025-03-04 07:45:42 +0100

    feat: enhance event creation form with category and access type selection, integrate image upload functionality

--- src/modules/OrgModules/pages/Events/components/aboutEvent.jsx
+++ src/modules/OrgModules/pages/Events/components/aboutEvent.jsx
@@ -3,51 +3,98 @@ import Checkbox from "../../../../../components/CheckBox";
 import DropZone from "../../../../../components/DropZone";
 import Input from "../../../../../components/Input";
 import TextArea from "../../../../../components/TextArea";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Button } from "@material-tailwind/react";
+import useApiMutation from "../../../../../api/hooks/useApiMutation";
 
-export default function AboutEvent({next}) {
-    const { register, formState: { errors } } = useForm();
-    const [isLoading, setIsLoading] = useState(false);
-    const [fileLoading, setFileLoading] = useState(false);
-    const [files, setFiles] = useState([]);
+export default function AboutEvent({ next }) {
+    const eventPayload = JSON.parse(localStorage.getItem('eventPayload'));
+    const event = eventPayload ? eventPayload : null;
 
-    const handleClick = () => {
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [files, setFiles] = useState(event ? [event.image] : []);
+    const [eventCategories, setEventCategories] = useState([]);
+    const [aboutEvents] = useState(event);
+
+    const { mutate } = useApiMutation();
+
+    const accessType = [
+        { label: 'Public', value: 'Public' },
+        { label: 'Private', value: 'Private' },
+        { label: 'Semi-private', value: 'Semi-private' }
+    ]
+
+    const getCategories = () => {
+        mutate({
+            url: `/api/events/event/categories`,
+            method: "GET",
+            headers: true,
+            hideToast: true,
+            onSuccess: (response) => {
+                setEventCategories(response.data.data.map((category) => {
+                    return { label: category.name, value: category.id }
+                }))
+            },
+            onError: () => {
+            }
+        });
+    }
+
+
+    useEffect(() => {
+        getCategories()
+    }, []);
+
+
+    const handleDrop = (data) => {
+        setFiles((prevFiles) => [data]);
+    }
+
+
+    const createEvent = (data) => {
+        const payload = {
+            ...aboutEvents,
+            ...data,
+            category: Number(data.category),
+            image: files[0]
+        };
+        localStorage.setItem('eventPayload', JSON.stringify(payload));
         next(true);
     }
 
+
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit(createEvent)}>
                 <div className="mb-1 flex flex-col gap-10 mt-5">
                     <div className="flex flex-col w-full gap-6">
                         <p className="-mb-3 text-mobiFormGray">
                             Event Name
                         </p>
-                        <Input type="text" name="firstName" register={register} placeholder="Event Name" />
+                        <Input type="text" name="name" value={aboutEvents?.name} rules={{ required: 'Event Name is required' }} errors={errors} register={register} placeholder="Event Name" />
                     </div>
 
                     <div className="flex flex-col w-full gap-6">
                         <p className="-mb-3 text-mobiFormGray">
                             Description
                         </p>
-                        <TextArea name="lastName" register={register} placeholder="Tell us about your event" />
+                        <TextArea name="description" value={aboutEvents?.description} errors={errors} rules={{ required: 'Event Description is required' }} register={register} placeholder="Tell us about your event" />
                     </div>
 
                     <div className="flex flex-col w-full gap-6">
                         <p className="-mb-3 text-mobiFormGray">
                             Event Category
                         </p>
-                        <Input name="phoneNumber" register={register}
-                            type="text" placeholder="Event Category" />
+                        <Input name="category" value={aboutEvents?.category} options={eventCategories} rules={{ required: 'Event Category is required' }} type='select' register={register}
+                            placeholder="Event Category" />
                     </div>
 
                     <div className="flex flex-col w-full gap-6">
                         <p className="-mb-3 text-mobiFormGray">
                             Access Type
                         </p>
-                        <Input name="phoneNumber" register={register}
-                            type="text" placeholder="Choose your Event Access Type" />
+                        <Input name="accessType" value={aboutEvents?.accessType} register={register} rules={{ required: 'Event access type is required' }}
+                            type="select" options={accessType} placeholder="Choose your Event Access Type" />
                     </div>
 
                     <div className="w-full flex lg:flex-row md:flex-row flex-col gap-6">
@@ -56,7 +103,7 @@ export default function AboutEvent({next}) {
                                 <p className="-mb-3 text-mobiFormGray">
                                     Event Image
                                 </p>
-                                <DropZone text="Upload Event Image" />
+                                <DropZone text="Upload Event Image" onUpload={handleDrop} multiple={false} />
                             </div>
                             <div className="grid grid-cols-3 gap-4 mt-4">
                                 {files.map((fileObj, index) => (
@@ -76,11 +123,11 @@ export default function AboutEvent({next}) {
                         <div className="flex gap-1">
                             <span className="flex">
                                 <Checkbox
-                                    name="acceptedTnC"
+                                    name="allowVerifierRequests"
                                     label="Mark Event as Open for Verifiers"
                                     register={register}
-                                    rules={{ required: 'Terms & Conditions is required' }}
                                     errors={errors}
+                                    value={event?.allowVerifierRequests}
                                 />
                             </span>
                         </div>
@@ -88,8 +135,8 @@ export default function AboutEvent({next}) {
 
 
                     <div className="flex">
-                        <Button onClick={() => handleClick()} disabled={isLoading} className="bg-mobiPink md:w-1/4 w-full p-3 rounded-full">
-                            {isLoading ? 'Updating...' : 'Next'}
+                        <Button type="submit" className="bg-mobiPink md:w-1/4 w-full p-3 rounded-full">
+                            Next
                         </Button>
                     </div>
                 </div>
--- src/modules/OrgModules/pages/Events/components/locationEvent.jsx
+++ src/modules/OrgModules/pages/Events/components/locationEvent.jsx
@@ -3,33 +3,70 @@ import DropZone from "../../../../../components/DropZone";
 import Input from "../../../../../components/Input";
 import { useState } from "react";
 import { Button } from "@material-tailwind/react";
+import { FaTimes } from "react-icons/fa";
 
-export default function LocationEvent({next}) {
-    const { register, formState: { errors } } = useForm();
-    const [isLoading, setIsLoading] = useState(false);
-    const [fileLoading, setFileLoading] = useState(false);
-    const [files, setFiles] = useState([]);
+export default function LocationEvent({ next, back }) {
+    const eventPayload = JSON.parse(localStorage.getItem('eventPayload'));
+    const event = eventPayload ? eventPayload : null;
 
-    const handleClick = () => {
-        next(true);
+    const { register, handleSubmit, formState: { errors } } = useForm();
+    const [files, setFiles] = useState(event.venueImage ? event.venueImage : []);
+
+
+    const handleBack = () => {
+        back(true)
     }
+
+
+    const handleDrop = (data) => {
+        setFiles((prevFiles) => [...prevFiles, data]);
+    }
+
+
+    const removeImage = (indexToRemove) => {
+        setFiles((prevFiles) => prevFiles.filter((_, index) => index !== indexToRemove));
+    };
+
+
+    const createEvent = (data) => {
+        const payload = {
+             ...event,
+             ...data,
+            venue: {
+                name: data.venueName,
+                address: `${data.street} ${data.city} ${data.state}, ${data.country}`
+            },
+            venueImage: files
+         }; 
+         localStorage.setItem('eventPayload', JSON.stringify(payload));
+         next(true);
+    }
+
+    console.log(event.startDate)
 
     return (
         <>
-            <form>
+            <form onSubmit={handleSubmit(createEvent)}>
                 <div className="mb-1 flex flex-col gap-10 mt-5">
                     <div className="flex flex-col w-full gap-6">
                         <p className="-mb-3 text-mobiFormGray">
-                            Venue (Location)
+                            Name of Venue
                         </p>
-                        <Input type="text" name="firstName" register={register} placeholder="Enter the venue of your event" />
+                        <Input type="text" name="venueName" value={event?.venueName} register={register} rules={{ required: 'Name of Venue is required' }} placeholder="Enter the name of the venue of your event" />
+                    </div>
+
+                    <div className="flex flex-col w-full gap-6">
+                        <p className="-mb-3 text-mobiFormGray">
+                            Venue (Address)
+                        </p>
+                        <Input type="text" name="street" value={event?.street} rules={{ required: 'Address is required' }} register={register} placeholder="Enter the address of the venue of your event" />
                     </div>
 
                     <div className="flex flex-col w-full gap-6">
                         <p className="-mb-3 text-mobiFormGray">
                             Country
                         </p>
-                        <Input type="text" name="firstName" register={register} placeholder="Choose the country" />
+                        <Input type="text" value={event?.country} rules={{ required: 'Country is required' }} errors={errors} name="country" register={register} placeholder="Enter country" />
                     </div>
 
                     <div className="w-full flex lg:flex-row md:flex-row flex-col gap-6">
@@ -37,25 +74,17 @@ export default function LocationEvent({next}) {
                             <p className="-mb-3 text-mobiFormGray">
                                 State
                             </p>
-                            <Input name="phoneNumber" register={register}
-                                type="text" placeholder="Choose the State" />
+                            <Input name="state" value={event?.state} rules={{ required: 'State is required' }} errors={errors} register={register}
+                                type="text" placeholder="Enter State" />
                         </div>
 
                         <div className="flex flex-col w-full gap-6">
                             <p className="-mb-3 text-mobiFormGray">
                                 City
                             </p>
-                            <Input name="phoneNumber" register={register}
-                                type="text" placeholder="Choose the City" />
+                            <Input name="city" value={event?.city} rules={{ required: 'City is required' }} errors={errors} register={register}
+                                type="text" placeholder="Enter City" />
                         </div>
-                    </div>
-
-                    <div className="flex flex-col w-full gap-6">
-                        <p className="-mb-3 text-mobiFormGray">
-                            Access Type
-                        </p>
-                        <Input name="phoneNumber" register={register}
-                            type="text" placeholder="Choose your Event Access Type" />
                     </div>
 
                     <div className="w-full flex lg:flex-row md:flex-row flex-col gap-6">
@@ -64,7 +93,7 @@ export default function LocationEvent({next}) {
                                 <p className="-mb-3 text-mobiFormGray">
                                     Venue Photos (Optional)
                                 </p>
-                                <DropZone text="Upload Venue Photo" />
+                                <DropZone text="Upload Venue Photo" onUpload={handleDrop} />
                             </div>
                             <div className="grid grid-cols-3 gap-4 mt-4">
                                 {files.map((fileObj, index) => (
@@ -74,54 +103,45 @@ export default function LocationEvent({next}) {
                                             alt="preview"
                                             className="w-full h-24 object-cover rounded"
                                         />
+                                        <button
+                                            onClick={() => removeImage(index)}
+                                            className="absolute top-1 right-1 bg-white shadow-lg text-black rounded-full p-1"
+                                        >
+                                            <FaTimes className="w-4 h-4" />
+                                        </button>
                                     </div>
                                 ))}
                             </div>
                         </div>
                     </div>
 
-                    <div className="w-full mt-10 border border-2 h-[1px]" />
+                    <div className="w-full my-10 border border-2 h-[1px]" />
 
-                    <div className="w-full flex lg:flex-row md:flex-row flex-col gap-6 mt-6 mb-6">
+                    <div className="w-full flex lg:flex-row md:flex-row flex-col gap-6">
                         <div className="flex flex-col w-full gap-6">
                             <p className="-mb-3 text-mobiFormGray">
                                 Start Date
                             </p>
-                            <Input name="phoneNumber" register={register}
-                                type="date" placeholder="Choose the Start date" />
+                            <Input name="startDate" value={event?.startDate} rules={{ required: 'Start Date is required' }} errors={errors} register={register}
+                                type="datetime-local" placeholder="Choose the Start date" />
                         </div>
 
                         <div className="flex flex-col w-full gap-6">
                             <p className="-mb-3 text-mobiFormGray">
                                 End Date
                             </p>
-                            <Input name="phoneNumber" register={register}
-                                type="date" placeholder="Choose the End date" />
+                            <Input name="endDate" value={event?.endDate} rules={{ required: 'End Date is required' }} errors={errors} register={register}
+                                type="datetime-local" placeholder="Choose the End date" />
                         </div>
                     </div>
 
-                    <div className="w-full mt-10 border border-2 h-[1px]" />
+                    <div className="flex gap-8">
+                        <Button onClick={() => handleBack()} className="md:w-1/4 w-full p-3 rounded-full">
+                            Previous
+                        </Button>
 
-                    <div className="flex flex-col w-full gap-6 mt-6">
-                        <p className="-mb-3 text-mobiFormGray">
-                            Country
-                        </p>
-                        <Input name="phoneNumber" register={register}
-                            type="text" placeholder="Choose a reoccurence pattern" />
-                    </div>
-
-                    <div className="flex flex-col w-full gap-6">
-                        <p className="-mb-3 text-mobiFormGray">
-                            Number of Times
-                        </p>
-                        <Input name="phoneNumber" register={register}
-                            type="text" placeholder="Enter numbe rof reoccuring times" />
-                    </div>
-
-
-                    <div className="flex mt-5">
-                        <Button onClick={() => handleClick()} disabled={isLoading} className="bg-mobiPink md:w-1/4 w-full p-3 rounded-full">
-                            {isLoading ? 'Updating...' : 'Next'}
+                        <Button type="submit" className="bg-mobiPink md:w-1/4 w-full p-3 rounded-full">
+                            Next
                         </Button>
                     </div>
                 </div>
