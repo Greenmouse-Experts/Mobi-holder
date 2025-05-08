0918bf019b176eb0c3d976cce3894d256d1624d7
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2025-05-08 19:08:12 +0100

    feat: Add profile photo upload functionality to AccountInfo component

--- src/modules/SuperAdmin/Settings/tabs/accountInfo.jsx
+++ src/modules/SuperAdmin/Settings/tabs/accountInfo.jsx
@@ -1,20 +1,44 @@
 import { useForm } from "react-hook-form";
 import useApiMutation from "../../../../api/hooks/useApiMutation";
 import { useDispatch, useSelector } from "react-redux";
-import { useState } from "react";
+import { useRef, useState } from "react";
 import Input from "../../../../components/Input";
 import { Button } from "@material-tailwind/react";
 import { setUser } from "../../../../reducers/userSlice";
+import AvatarInitials from "../../../../components/AvatarInitials";
+import useFileUpload from "../../../../api/hooks/useFileUpload";
 
 export default function AccountInfo() {
     const user = useSelector((state) => state.userData.data);
-    const { register, handleSubmit, setValue, watch, formState: { errors } } =
+    const { register, handleSubmit, setValue, getValues, watch, formState: { errors } } =
         useForm();
 
+    const fileInputRef = useRef(null);
     const [isLoading, setIsLoading] = useState(false);
+    const [profilePhoto, setProfilePhoto] = useState(null);
+    const { uploadFiles, isLoadingUpload } = useFileUpload();
     const dispatch = useDispatch();
     const { mutate } = useApiMutation();
 
+
+    const handleButtonClick = () => {
+        // Simulate a click on the hidden file input
+        if (fileInputRef.current) {
+            fileInputRef.current.click();
+        }
+    };
+
+    const handleFileChange = async (event) => {
+        const files = event.target.files;
+        if (files.length > 0) {
+            await uploadFiles(files, (uploadedUrl) => {
+                setProfilePhoto(uploadedUrl);
+                const currentValues = getValues(); // Get current form data
+                changeInfo({ ...currentValues, photo: uploadedUrl });
+            });
+        }
+    };
+    
 
     const changeInfo = (data) => {
         setIsLoading(true);
@@ -35,6 +59,31 @@ export default function AccountInfo() {
     return (
         <div className="mb-1 flex flex-col gap-5 mt-5">
             <form onSubmit={handleSubmit(changeInfo)} className="flex flex-col gap-5">
+                <div className="flex md:flex-row flex-col gap-3">
+                    {user.photo ?
+                        <div className="flex w-32 h-32">
+                            <img src={`${user.photo}`} className="w-full h-full object-cover rounded-full" />
+                        </div>
+                        :
+                        <AvatarInitials name={`${user.firstName}${user.lastName}`} size="32" />
+                    }
+                    <div className="flex flex-col justify-center md:mx-3">
+                        <input
+                            type="file"
+                            ref={fileInputRef}
+                            onChange={handleFileChange}
+                            style={{ display: "none" }}
+                            multiple
+                        />
+                        <Button className="bg-transparent px-7 rounded-full chartColor border-[0.5px] border-gray-700"
+                            onClick={handleButtonClick}
+                            disabled={isLoadingUpload}
+                        >
+                            {isLoadingUpload ? 'Changing Picture' : 'Change Picture'}
+                        </Button>
+                    </div>
+                </div>
+
                 <div className="w-full flex lg:flex-row md:flex-row flex-col gap-6">
                     <div className="flex flex-col w-full gap-6">
                         <p className="-mb-3 text-mobiFormGray">
