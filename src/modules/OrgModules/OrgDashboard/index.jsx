46a89f74a2285a0ccd688f30f236b0180784a634
Anyanwu Chukwuemeka <anyanwue4@gmail.com>
2025-06-06 16:10:10 +0100

    feat: remove Action column from TableHeaders and NewTableHeaders, add hidePagination prop to tables

--- src/modules/OrgModules/OrgDashboard/index.jsx
+++ src/modules/OrgModules/OrgDashboard/index.jsx
@@ -11,9 +11,9 @@ import Loader from "../../../components/Loader";
 import { dateFormat } from "../../../helpers/dateHelper";
 
 
-const TableHeaders = ["Individuals", "Role", "Date", "Status", "Action"];
+const TableHeaders = ["Individuals", "Role", "Date", "Status"];
 
-const NewTableHeaders = ["Event Name", "Ticket Type", "Start Date", "End Date", "Action"];
+const NewTableHeaders = ["Event Name", "Ticket Type", "Start Date", "End Date"];
 
 
 export default function OrgDashboard() {
@@ -142,6 +142,7 @@ export default function OrgDashboard() {
                     <div className="w-full flex lg:flex-row md:flex-row flex-col gap-5 my-2">
                         <div className="lg:w-[63%] md:w-[63%] w-full flex flex-col gap-5">
                             <Table title="Today" subTitle={<span>New Members</span>} filter exportData
+                                hidePagination
                                 tableBtn={<button className="bg-mobiPink text-white px-2 py-1 rounded-md">Add New User</button>}
                                 tableHeader={TableHeaders}>
                                 {allMembers.length > 0 ?
@@ -151,13 +152,6 @@ export default function OrgDashboard() {
                                             <td className="px-3 py-3 text-mobiTableText">{data.designation}</td>
                                             <td className="px-3 py-3 text-mobiTableText">{dateFormat(data.createdAt, 'dd-MM-yyy')}</td>
                                             <td className="px-3 py-3 text-mobiTableText"><Badge status={data.status} /></td>
-                                            <td className="px-3 py-3">
-                                                <span className="flex w-full justify-center">
-                                                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                        <path d="M21 12L9 12M21 6L9 6M21 18L9 18M5 12C5 12.5523 4.55228 13 4 13C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11C4.55228 11 5 11.4477 5 12ZM5 6C5 6.55228 4.55228 7 4 7C3.44772 7 3 6.55228 3 6C3 5.44772 3.44772 5 4 5C4.55228 5 5 5.44772 5 6ZM5 18C5 18.5523 4.55228 19 4 19C3.44772 19 3 18.5523 3 18C3 17.4477 3.44772 17 4 17C4.55228 17 5 17.4477 5 18Z" stroke="#AEB9E1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                                    </svg>
-                                                </span>
-                                            </td>
                                         </tr>
                                     ))
                                     :
@@ -190,7 +184,7 @@ export default function OrgDashboard() {
                         </div>
 
                         <div className="lg:w-[65%] md:w-[65%] w-full flex flex-col gap-5">
-                            <Table title="Today" subTitle={<span>Upcoming Events</span>} filter exportData
+                            <Table title="Today" subTitle={<span>Upcoming Events</span>} filter exportData hidePagination
                                 tableHeader={NewTableHeaders}>
                                 {events.length > 0 ?
                                     events
@@ -200,13 +194,6 @@ export default function OrgDashboard() {
                                                 <td className="px-3 py-3 text-mobiTableText">{data.ticketType}</td>
                                                 <td className="px-3 py-3 text-mobiTableText">{dateFormat(data.startDate, 'dd-MM-yyy')}</td>
                                                 <td className="px-3 py-3 text-mobiTableText">{dateFormat(data.endDate, 'dd-MM-yyy')}</td>
-                                                <td className="px-3 py-3">
-                                                    <span className="flex w-full justify-center">
-                                                        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                                            <path d="M21 12L9 12M21 6L9 6M21 18L9 18M5 12C5 12.5523 4.55228 13 4 13C3.44772 13 3 12.5523 3 12C3 11.4477 3.44772 11 4 11C4.55228 11 5 11.4477 5 12ZM5 6C5 6.55228 4.55228 7 4 7C3.44772 7 3 6.55228 3 6C3 5.44772 3.44772 5 4 5C4.55228 5 5 5.44772 5 6ZM5 18C5 18.5523 4.55228 19 4 19C3.44772 19 3 18.5523 3 18C3 17.4477 3.44772 17 4 17C4.55228 17 5 17.4477 5 18Z" stroke="#AEB9E1" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-                                                        </svg>
-                                                    </span>
-                                                </td>
                                             </tr>
                                         ))
                                     :
