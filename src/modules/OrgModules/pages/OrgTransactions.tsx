c9d5d4e9c86e189b009ece768878f71a67c7b4ef
destiny <desto4q@gmail.com>
2025-08-22 10:59:13 +0100

    org transactions

--- src/modules/OrgModules/pages/OrgTransactions.tsx
+++ src/modules/OrgModules/pages/OrgTransactions.tsx
@@ -109,16 +109,32 @@ export default function OrgTransactions() {
     return (
       <>
         <Header mobile organisation data={user} title={"Designation"} />
-        <div className="mt-6">
-          <div className="mt-4">
-            <p>Error Occurd</p>
-            <button
-              className="mt-4 px-4 py-2 bg-red-600 text-white rounded hover:bg-red-700 transition-colors"
-              onClick={() => query.refetch()}
+        <div className="flex flex-col items-center justify-center mt-8 p-4 min-h-[520px] rounded-lg shadow-md">
+          <div className="mb-4">
+            <svg
+              className="w-12 h-12 text-red-500 mx-auto"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              Retry
-            </button>
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+              ></path>
+            </svg>
+            <p className="text-red-700 text-center font-semibold">
+              An error occurred!
+            </p>
           </div>
+          <button
+            className="px-6 py-3 bg-red-500 text-white rounded-md hover:bg-red-600 focus:outline-none focus:ring-2 focus:ring-red-400 transition-colors duration-200"
+            onClick={() => query.refetch()}
+          >
+            Try Again
+          </button>
         </div>
       </>
     );
--- src/routes/SuperAdminRoutes.jsx
+++ src/routes/SuperAdminRoutes.jsx
@@ -2,11 +2,11 @@ import { Route } from "react-router-dom";
 import DashBoard from "../modules/SuperAdmin/DashBoard";
 import Organisations from "../modules/SuperAdmin/Organisations";
 import Events from "../modules/SuperAdmin/Events";
-import AllUsers from '../modules/SuperAdmin/Users';
-import Subscriptions from '../modules/SuperAdmin/Subscriptions';
-import IDCards from '../modules/SuperAdmin/IDCards';
-import ViewOrgAdmin from '../modules/SuperAdmin/Organisations/viewOrganisation';
-import ViewUserAdmin from '../modules/SuperAdmin/Users/viewUser';
+import AllUsers from "../modules/SuperAdmin/Users";
+import Subscriptions from "../modules/SuperAdmin/Subscriptions";
+import IDCards from "../modules/SuperAdmin/IDCards";
+import ViewOrgAdmin from "../modules/SuperAdmin/Organisations/viewOrganisation";
+import ViewUserAdmin from "../modules/SuperAdmin/Users/viewUser";
 //import EventLog from "../modules/SuperAdmin/Events/modules/eventLog";
 import EventTickets from "../modules/SuperAdmin/Events/modules/eventVerifiers";
 import EventsCategories from "../modules/SuperAdmin/Events/eventsCategories";
@@ -40,49 +40,72 @@ import UserInquiries from "../modules/SuperAdmin/UserInquiries";
 import NewsletterSubscribers from "../modules/SuperAdmin/NewsLetter";
 import Notification from "../modules/SuperAdmin/Notifications";
 import BLog from "../modules/SuperAdmin/Blog/index.jsx";
+import OrgTransactions from "../modules/OrgModules/pages/OrgTransactions";
 const SuperAdminRoutes = [
-    <Route path="dashboard" element={<DashBoard />} />,
-    <Route path="organisation" element={<Organisations />} />,
-    <Route path="organisation/members/:id" element={<OrgMembers />} />,
-    <Route path="organisation/templates/:id" element={<OrgTemplates />} />,
-    <Route path="individuals" element={<AllUsers />} />,
-    <Route path="view-individual/:id" element={<ViewUserAdmin />} />,
-    <Route path="view-org/:id" element={<ViewOrgAdmin />} />,
-    <Route path="membership" element={<Members />} />,
-    <Route path="events" element={<Events />} />,
-    <Route path="events/categories" element={<EventsCategories />} />,
-    <Route path="subscription-individual" element={<Subscriptions />} />,
-    <Route path="subscription-individual/plans" element={<IndividualPlan />} />,
-    <Route path="subscription-organisation/plans" element={<OrganisationPlan />} />,
-    <Route path="subscription-individual/plans/create" element={<CreatePlan />} />,
-    <Route path="subscription-organisation/plans/create" element={<OrgCreatePlan />} />,
-    <Route path="subscription-individual/plans/edit/:id" element={<CreatePlan />} />,
-    <Route path="subscription-organisation/plans/edit/:id" element={<OrgCreatePlan />} />,
-    <Route path="subscription-log/individual" element={<IndividualLog />} />,
-    <Route path="subscription-log/organisation" element={<OrganisationLog />} />,
-    <Route path="personal-idCards" element={<AllPersonalIDCards />} />,
-    <Route path="personal-idCards/view-personal-card/:id" element={<ViewPersonalCard />} />,
-    <Route path="idCardsTemplate" element={<AllTemplates />} />,
-    <Route path="idCardsTemplate/view/:id" element={<ViewTemplate />} />,
-    <Route path="id-cards" element={<IDCards />} />,
-    <Route path="id-cards/view/:id" element={<AdminViewCard />} />,
-    <Route path="events/verifiers/:id/:eventId" element={<EventVerifiers />} />,
-    <Route path="events/event-ticket/:id" element={<EventTickets />} />,
-    <Route path="events/view/:id" element={<ViewEvent />} />,
-    <Route path="events/event-attendees/:id/:eventId" element={<EventAttendees />} />,
-    <Route path="verifiers" element={<AllVerifiers />} />,
-    <Route path="staffs" element={<Staffs />} />,
-    <Route path="staffs/create" element={<AddStaff />} />,
-    <Route path="staffs/update/:id" element={<UpdateStaff />} />,
-    <Route path="staffs/roles" element={<ViewRoles />} />,
-    <Route path="settings" element={<AdminSettings />} />,
-    <Route path="faqs" element={<FAQs />} />,
-    <Route path="faqs/categories" element={<FAQCategories />} />,
-    <Route path="activityLog" element={<StaffActivityLog />} />,
-    <Route path="user-inquiries" element={<UserInquiries />} />,
-    <Route path="newsletter" element={<NewsletterSubscribers />} />,
-    <Route path="notification" element={<Notification />} />,
-    <Route path="blog" element={<BLog />} />
+  <Route path="dashboard" element={<DashBoard />} />,
+  <Route path="organisation" element={<Organisations />} />,
+  <Route path="organisation/members/:id" element={<OrgMembers />} />,
+  <Route path="organisation/templates/:id" element={<OrgTemplates />} />,
+  <Route path="individuals" element={<AllUsers />} />,
+  <Route path="view-individual/:id" element={<ViewUserAdmin />} />,
+  <Route path="view-org/:id" element={<ViewOrgAdmin />} />,
+  <Route path="membership" element={<Members />} />,
+  <Route path="events" element={<Events />} />,
+  <Route path="events/categories" element={<EventsCategories />} />,
+  <Route path="subscription-individual" element={<Subscriptions />} />,
+  <Route path="subscription-individual/plans" element={<IndividualPlan />} />,
+  <Route
+    path="subscription-organisation/plans"
+    element={<OrganisationPlan />}
+  />,
+  <Route
+    path="subscription-individual/plans/create"
+    element={<CreatePlan />}
+  />,
+  <Route
+    path="subscription-organisation/plans/create"
+    element={<OrgCreatePlan />}
+  />,
+  <Route
+    path="subscription-individual/plans/edit/:id"
+    element={<CreatePlan />}
+  />,
+  <Route
+    path="subscription-organisation/plans/edit/:id"
+    element={<OrgCreatePlan />}
+  />,
+  <Route path="subscription-log/individual" element={<IndividualLog />} />,
+  <Route path="subscription-log/organisation" element={<OrganisationLog />} />,
+  <Route path="personal-idCards" element={<AllPersonalIDCards />} />,
+  <Route
+    path="personal-idCards/view-personal-card/:id"
+    element={<ViewPersonalCard />}
+  />,
+  <Route path="idCardsTemplate" element={<AllTemplates />} />,
+  <Route path="idCardsTemplate/view/:id" element={<ViewTemplate />} />,
+  <Route path="id-cards" element={<IDCards />} />,
+  <Route path="id-cards/view/:id" element={<AdminViewCard />} />,
+  <Route path="events/verifiers/:id/:eventId" element={<EventVerifiers />} />,
+  <Route path="events/event-ticket/:id" element={<EventTickets />} />,
+  <Route path="events/view/:id" element={<ViewEvent />} />,
+  <Route
+    path="events/event-attendees/:id/:eventId"
+    element={<EventAttendees />}
+  />,
+  <Route path="verifiers" element={<AllVerifiers />} />,
+  <Route path="staffs" element={<Staffs />} />,
+  <Route path="staffs/create" element={<AddStaff />} />,
+  <Route path="staffs/update/:id" element={<UpdateStaff />} />,
+  <Route path="staffs/roles" element={<ViewRoles />} />,
+  <Route path="settings" element={<AdminSettings />} />,
+  <Route path="faqs" element={<FAQs />} />,
+  <Route path="faqs/categories" element={<FAQCategories />} />,
+  <Route path="activityLog" element={<StaffActivityLog />} />,
+  <Route path="user-inquiries" element={<UserInquiries />} />,
+  <Route path="newsletter" element={<NewsletterSubscribers />} />,
+  <Route path="notification" element={<Notification />} />,
+  <Route path="transactions" element={<OrgTransactions />} />,
+  <Route path="blog" element={<BLog />} />,
 ];
 
 export default SuperAdminRoutes;
